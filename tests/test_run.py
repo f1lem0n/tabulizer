427d56698805fb7bed05afa019d275c34427a558
Filip Hajdyła <hajdylaf@gmail.com>
2023-02-12 02:00:08 +0100

    cleaned up test outputs

--- tests/test_run.py
+++ tests/test_run.py
@@ -3,10 +3,9 @@ import os
 
 import tabulizer.main as main
 
-output = "sample_output/multicol.csv"
-input = ["sample_data/random2.csv",
-         "sample_data/random3.tsv",
-         "sample_data/random1.xls"]
+inputs = ["sample_data/random2.csv",
+          "sample_data/random3.tsv",
+          "sample_data/random1.xls"]
 colnames = ["The",
             "Holy",
             "Grail"]
@@ -15,10 +14,11 @@ colnames = ["The",
 class TestRun():
 
     def test_run_unnamed_cols(self):
+        output = "sample_output/multicol_unnamed.csv"
         file_exists = os.path.exists(output)
         if file_exists:
             os.remove(output)
-        args = argparse.Namespace(input=input,
+        args = argparse.Namespace(input=inputs,
                                   output=output,
                                   colnames=None,
                                   func=main.run)
@@ -28,13 +28,14 @@ class TestRun():
         with open(output) as f:
             rows = f.readlines()
         for row in rows:
-            assert row.count(",") == len(input) - 1
+            assert row.count(",") == len(inputs) - 1
 
     def test_run_named_cols(self):
+        output = "sample_output/multicol_named.csv"
         file_exists = os.path.exists(output)
         if file_exists:
             os.remove(output)
-        args = argparse.Namespace(input=input,
+        args = argparse.Namespace(input=inputs,
                                   output=output,
                                   colnames=colnames,
                                   func=main.run)
@@ -44,13 +45,14 @@ class TestRun():
         with open(output) as f:
             rows = f.readlines()
         for row in rows:
-            assert row.count(",") == len(input) - 1
+            assert row.count(",") == len(inputs) - 1
 
     def test_run_partially_named_cols(self):
+        output = "sample_output/multicol_part.csv"
         file_exists = os.path.exists(output)
         if file_exists:
             os.remove(output)
-        args = argparse.Namespace(input=input,
+        args = argparse.Namespace(input=inputs,
                                   output=output,
                                   colnames=colnames[:2],
                                   func=main.run)
@@ -60,4 +62,4 @@ class TestRun():
         with open(output) as f:
             rows = f.readlines()
         for row in rows:
-            assert row.count(",") == len(input) - 1
+            assert row.count(",") == len(inputs) - 1
